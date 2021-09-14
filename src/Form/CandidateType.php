e2245bf45c278b6a2bc7f8b0a8d0993941025cf3
jojogames632 <jojogames@free.fr>
2021-09-14 10:23:04 +0200

    Add max length constraints to secure candidate and recruiter edit profil forms

--- src/Form/CandidateType.php
+++ src/Form/CandidateType.php
@@ -7,15 +7,34 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class CandidateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('lastName', TextType::class)
-            ->add('cvFilename', TextType::class)
+            ->add('firstName', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('cvFilename', TextType::class, [
+                'required' => false
+            ])
         ;
     }
 
--- src/Form/RecruiterType.php
+++ src/Form/RecruiterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RecruiterType extends AbstractType
@@ -13,8 +14,24 @@ class RecruiterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('company', TextType::class)
-            ->add('address', TextType::class)
+            ->add('company', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'Votre mot de passe doit contenir au maximum {{ limit }} caractères',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
         ;
     }
 
